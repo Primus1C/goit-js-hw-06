312722d4bc0990a58938f3bc313fc8c3e8d0de99
Artur Rakhimov <kharkiv1c@gmail.com>
2023-05-21 17:36:03 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+const arr = [];
+ingredients.forEach((element) => {
+  const li = document.createElement("li");
+  li.textContent = element;
+  li.classList.add("item");
+  arr.push(li);
+});
+ulEl.append(...arr);
