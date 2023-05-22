5e9e73c3dc67f6a0f15ec3783a693f6ca503f9f3
Artur Rakhimov <kharkiv1c@gmail.com>
2023-05-22 20:20:56 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const submitFrm = document.querySelector(".login-form");
+submitFrm.addEventListener("submit", (evt)=>{evt.preventDefault()});
 
+const submitBtn = document.querySelector(".login-form button[type=submit]");
+submitBtn.addEventListener("click", onSubmit);
+
+const inputArray = document.querySelectorAll(".login-form input");
+
+function onSubmit(evt) {
+
+    const result = {};
+    let fillErr = false;
+    inputArray.forEach((element) => {
+        if (element.value == "") { fillErr = true };
+        result[element.name] = element.value;
+    });
+
+    if (fillErr) {
+        alert("Все поля регистрации должны быть заполнены!");
+    } else {
+        console.log(result);
+    }
+}
