b3db20501bc9a7601e8f2137782ee778b1cebf64
Artur <110535829+Primus1C@users.noreply.github.com>
2023-05-22 06:25:42 +0200

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,18 @@
 let counterValue = 0;
- 
+const valueEl = document.querySelector("#value");
+const btnMinus = document.querySelector("button[data-action=decrement]");
+const btnPlus = document.querySelector("button[data-action=increment]");
+
+btnMinus.addEventListener("click", onClickBtnMinus);
+btnPlus.addEventListener("click", onClickBtnPlus);
+
+function onClickBtnMinus(evt) {
+    counterValue -=1;
+    valueEl.innerText = counterValue;
+}
+
+function onClickBtnPlus(evt) {
+    counterValue += 1;
+    valueEl.innerText = counterValue;
+}
+
